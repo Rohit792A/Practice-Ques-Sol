f6dcb46efa5884dbdb2a896194c167fe8a548a7f
Rohit Kushwaha <73462607+Rohit792A@users.noreply.github.com>
2024-04-23 09:54:21 +0530

    Made changes to the code and added comments to  mapper.py

--- Hadoop/mapper.py
+++ Hadoop/mapper.py
@@ -10,15 +10,17 @@ for line in sys.stdin:
 	# split the line into words 
 	words = line.split() 
 		
-	# we are looping over the words array and printing the word 
-	# with the count of 1 to the STDOUT 
+	# As we have to only count the words so removing the punctuation marks
 	for word in words:
 		if word in [",",".","?","!"]:
 			pass
 		else:
 		
+		
+		# we are looping over the words array and printing the word 
+	 	# with the count of 1 to the STDOUT
+			print ('%s\t\t\t%s' % (word, 1) )
+			
 		# write the results to STDOUT (standard output); 
 		# what we output here will be the input for the 
 		# Reduce step, i.e. the input for reducer.py 
-			print ('%s\t\t\t%s' % (word, 1) )
-		
